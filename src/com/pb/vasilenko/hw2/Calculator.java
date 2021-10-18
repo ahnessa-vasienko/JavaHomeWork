7122d18d406dac738390acdf276f4417293e56a2
Agnessa Vasilenko <ahnessa.vasylenko@privatbank.ua>
2021-10-18 23:59:49 +0300

    init commit

--- src/com/pb/vasilenko/hw2/Calculator.java
+++ src/com/pb/vasilenko/hw2/Calculator.java
@@ -1,9 +1,8 @@
 package com.pb.vasilenko.hw2;
 
-import com.sun.xml.internal.ws.api.streaming.XMLStreamWriterFactory;
-
-
 import java.util.Scanner;
+
+import static java.lang.System.in;
 public class Calculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -13,28 +12,27 @@ public class Calculator {
         x = scan.nextInt();
         y = scan.nextInt();
         System.out.print("\nEnter an operator (+, -, *, /): ");
-        op = scan.next() .charAt(0);
-        switch(op) {
-        case '+':
-            System.out.println(+(x+y));
-        break;
-        case '-':
-            System.out.println(+(x-y));
-        break;
-        case '*':
-            System.out.println(+(x*y));
-        break;
-        case '/':
-            System.out.println(+(x/y));
-        if (y == 0) {
+        op = scan.next().charAt(0);
+        switch (op) {
+            case '+':
+                System.out.println(+(x + y));
+                break;
+            case '-':
+                System.out.println(+(x - y));
+                break;
+            case '*':
+                System.out.println(+(x * y));
+                break;
+            case '/':
+                if (y == 0)
+                    System.out.println("На 0 делить нельзя");
+                if (y != 0)
+                    System.out.println(+(x / y));
+                break;
 
-            System.out.println("на ноль не делить");
-            }
-        break;
             default: {
                 System.out.println("Неверная операция");
             }
-   }
- }
+        }
+    }
 }
-
