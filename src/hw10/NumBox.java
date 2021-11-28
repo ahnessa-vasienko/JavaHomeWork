c54c2c4610349d43f0fcbcc54b535ec0267e8ad2
Agnessa Vasilenko <ahnessa.vasylenko@privatbank.ua>
2021-11-28 23:46:24 +0200

    init commit

---
+++ src/hw10/NumBox.java
@@ -0,0 +1,9 @@
+package hw10;
+
+public class NumBox {
+        private Object object;
+        public void add(Object object)
+        { this.object = object; }
+        public Object get()
+        { return object; }
+}
